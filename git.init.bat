b9f0c98ee8d068bbb1b6df87ad14de1e97a94360
moh <osman.elrashid@gmail.com>
2019-08-04 06:20:38 +0400

    initial files

---
+++ git.init.bat
@@ -0,0 +1,5 @@
+Set HUB_PROTOCOL=ssh 
+git init 
+git add . && git commit -m "initial files" 
+hub create elrashid-flutter-examples/e025_flutter_positionedtransition_problem_a 
+git push --set-upstream origin master 
